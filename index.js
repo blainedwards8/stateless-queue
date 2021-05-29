4ee0c604a475238ede6e3fc1934b71b55f303f52
Blaine <blainedwards8@gmail.com>
2021-05-29 08:12:50 -0600

    Added a size and start method

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ class Queue {
       this.f = f;
       this.delay = delay;
   
-      this.timer = setInterval(async () => {
-        if(this.queue.length > 0){
-          let obj = this.queue.splice(0,1)[0];
-          await this.f(obj);
-        }
-      }, this.delay);
+      this.start();
     }
   
     add(obj) {
@@ -18,6 +13,19 @@ class Queue {
   
     stop () {
       clearInterval(this.timer);
+    }
+
+    start () {
+      this.timer = setInterval(async () => {
+        if(this.queue.length > 0){
+          let obj = this.queue.splice(0,1)[0];
+          await this.f(obj);
+        }
+      }, this.delay);
+    }
+
+    size () {
+      return this.queue.length;
     }
 }
 
